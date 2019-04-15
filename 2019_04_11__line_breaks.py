4e5c8fa0cf53af74f122dbd9b4c2a95ba861e1df
lschanne <lukeschanne@gmail.com>
2019-04-15 14:24:20 -0400

    Solve the problem from April 11, 2019

--- 2019_04_11__line_breaks.py
+++ 2019_04_11__line_breaks.py
@@ -14,3 +14,31 @@ and k = 10, you should return:
     ["the quick", "brown fox", "jumps over", "the lazy", "dog"]
 No string in the list has a length of more than 10.
 '''
+
+def line_break(string, k):
+    result = []
+    current_line = ''
+    for word in string.split():
+        if len(word) > k:
+            return None
+
+        if not current_line:
+            current_line = word
+        elif len(word) + 1 + len(current_line) <= k:
+            current_line = ' '.join((current_line, word))
+        else:
+            result.append(current_line)
+            current_line = word
+
+    result.append(current_line)
+
+    return result
+
+if __name__ == '__main__':
+    import sys
+    k = int(sys.argv[1])
+    for string in sys.argv[2:]:
+        print('input string: {}'.format(string))
+        print('k: {}'.format(k))
+        print('result: {}'.format(line_break(string, k)))
+        print('---------------------------------')
