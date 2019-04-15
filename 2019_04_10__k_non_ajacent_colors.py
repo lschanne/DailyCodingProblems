17f3faf5fe8b88688bffe117dd054fdff3cd000e
lschanne <lukeschanne@gmail.com>
2019-04-15 15:24:05 -0400

    Solve the problem for April 10, 2019

--- 2019_04_10__k_non_ajacent_colors.py
+++ 2019_04_10__k_non_ajacent_colors.py
@@ -7,8 +7,26 @@ such that no two adjacent vertices share the same color using at most k colors.
 '''
 
 def can_be_done(adjacency_matrix, k):
-    if not adjacency_matrix:
-        return True
+    if k <= 0:
+        return False
+
+    colors = [None for _ in range(len(adjacency_matrix))]
+    available_colors = [True for _ in range(k)]
+    for iii, row in enumerate(adjacency_matrix):
+        for kkk in range(k):
+            available_colors[kkk] = True
+
+        for jjj in range(iii):
+            if row[jjj]:
+                available_colors[colors[jjj]] = False
+
+        for kkk in range(k):
+            if available_colors[kkk]:
+                colors[iii] = kkk
+                break
+        else:
+            return False
+    return True
 
 if __name__ == '__main__':
     for adjacency_matrix in (
