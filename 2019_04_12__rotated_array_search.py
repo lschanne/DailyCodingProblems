241cabaede97c29a8b1577d7d6024a1f45149c15
lschanne <lukeschanne@gmail.com>
2019-04-15 14:51:55 -0400

    Solve problem from April 12, 2019

--- 2019_04_12__rotated_array_search.py
+++ 2019_04_12__rotated_array_search.py
@@ -11,3 +11,40 @@ For example, given the array [13, 18, 25, 2, 8, 10] and the element 8, return 4
 
 You can assume all the integers in the array are unique.
 '''
+
+def search(rotated_array, element):
+    if not rotated_array:
+        return None
+
+    left_idx = 0
+    right_idx = len(rotated_array) - 1
+    while True:
+        idx = (left_idx + right_idx) // 2
+        if rotated_array[idx] == element:
+            return idx
+        if left_idx == idx:
+            if rotated_array[right_idx] == element:
+                return right_idx
+            return None
+
+        if rotated_array[idx] > element:
+            if rotated_array[left_idx] <= element:
+                right_idx = idx
+            else:
+                left_idx = idx
+        else:
+            if rotated_array[right_idx] >= element:
+                left_idx = idx
+            else:
+                right_idx = idx
+
+if __name__ == '__main__':
+    import sys
+    rotated_array = list(map(int, sys.argv[1:]))
+    for element in (
+        [min(rotated_array) - 1] + rotated_array + [max(rotated_array) + 1]
+    ):
+        print('rotated array: {}'.format(rotated_array))
+        print('element: {}'.format(element))
+        print('index: {}'.format(search(rotated_array, element)))
+        print('--------------------------------')
