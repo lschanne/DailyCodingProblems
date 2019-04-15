b87dd73de33c118807e3b08c53ed5f137a30364e
lschanne <lukeschanne@gmail.com>
2019-04-15 15:05:01 -0400

    Solve the problem for April 14, 2019

--- 2019_04_14__same_sum_subsets.py
+++ 2019_04_14__same_sum_subsets.py
@@ -11,3 +11,24 @@ both add up to 55.
 Given the multiset {15, 5, 20, 10, 35}, it would return false, since we can't
 split it up into two subsets that add up to the same sum.
 '''
+
+def can_get_same_sum_subsets(multiset, sum_A=0, sum_B=0, non_empty=False):
+    if non_empty and sum_A == sum_B:
+        return True
+
+    for x in multiset:
+        new_set = multiset - {x}
+        if any((
+            can_get_same_sum_subsets(new_set, sum_A, sum_B, True),
+            can_get_same_sum_subsets(new_set, sum_A + x, sum_B, True),
+            can_get_same_sum_subsets(new_set, sum_A, sum_B + x, True),
+        )):
+            return True
+
+    return False
+
+if __name__ == '__main__':
+    import sys
+    multiset = set(map(int, sys.argv[1:]))
+    print('multiset: {}'.format(multiset))
+    print('result: {}'.format(can_get_same_sum_subsets(multiset)))
