113fd397acc7aa57e1f086525219739c9bbbb402
lschanne <lukeschanne@gmail.com>
2019-05-06 08:52:50 -0400

    Add problems for May 2-5, 2019

---
+++ 2019_05_02__sorted_list_merge.py
@@ -0,0 +1,6 @@
+'''
+May 2, 2019
+
+Given k sorted singly linked lists, write a function to merge all the lists
+into one sorted singly linked list.
+'''
---
+++ 2019_05_04__deepest_node.py
@@ -0,0 +1,12 @@
+'''
+May 4, 2019
+
+Given the root of a binary tree, return a deepest node. For example, in the
+following tree, return d.
+
+    a
+   / \
+  b   c
+ /
+d
+'''
