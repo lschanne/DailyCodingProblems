95e24b789b837d7781dd76e25e8e8983bf5608e9
lschanne <lukeschanne@gmail.com>
2019-05-14 13:16:31 -0400

    Solve problem for May 12, 2019

--- 2019_05_12__division_by_subtraction.py
+++ 2019_05_12__division_by_subtraction.py
@@ -5,3 +5,20 @@ Implement division of two positive integers without using the division,
 multiplication, or modulus operators. Return the quotient as an integer,
 ignoring the remainder.
 '''
+
+# This will break and/or not return correct results for negative numbers,
+# but that's in accordance with the problem statement
+def division_by_subtraction(numerator, divisor):
+    quotient = 0
+    while numerator >= divisor:
+        numerator -= divisor
+        quotient += 1
+    return quotient
+
+if __name__ == '__main__':
+    import sys
+    for idx in range(1, len(sys.argv), 2):
+        num, div = map(int, sys.argv[idx: idx + 2])
+        print('{} // {} = {}'.format(
+            num, div, division_by_subtraction(num, div)
+        ))
