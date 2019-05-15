4d24f28acef9b577d0b42f881b14fcd5e9fe8978
lschanne <lukeschanne@gmail.com>
2019-05-15 08:03:39 -0400

    Solve problem for May 10, 2019

--- 2019_05_10__min_edit_for_valid_parentheses.py
+++ 2019_05_10__min_edit_for_valid_parentheses.py
@@ -8,3 +8,29 @@ parenthesis is eventually closed).
 For example, given the string "()())()", you should return 1.
 Given the string ")(", you should return 2, since we must remove all of them.
 '''
+
+def min_edit_for_valid_parentheses(string):
+    balance = 0
+    count = 0
+    for char in string:
+        if char == '(':
+            balance += 1
+        elif char == ')':
+            balance -= 1
+            if balance < 0:
+                balance = 0
+                count += 1
+
+    balance += count
+    return balance
+
+if __name__ == '__main__':
+    import sys
+    for string in sys.argv[1:]:
+        print('input string: {}'.format(string))
+        print(
+            'minimum number of parentheses to be removed: {}'.format(
+                min_edit_for_valid_parentheses(string)
+            )
+        )
+        print('------------------------------------------')
