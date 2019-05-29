84250ec8305a6d8e761a1f92ee44b3bcfc85e527
lschanne <lukeschanne@gmail.com>
2019-05-29 08:43:58 -0400

    Solve problem for May 24, 2019

--- 2019_05_24__get_min_steps_to_point.py
+++ 2019_05_24__get_min_steps_to_point.py
@@ -22,3 +22,81 @@ Output: 2
 It takes 1 step to move from (0, 0) to (1, 1). It takes one more step to move
 from (1, 1) to (1, 2).
 '''
+
+def get_min_steps_to_point(points):
+    steps = 0
+    if not points:
+        return steps
+
+    prev_point = points[0]
+    for next_point in points[1:]:
+        steps += max(map(lambda i: abs(next_point[i] - prev_point[i]), (0, 1)))
+        prev_point = next_point
+
+    return steps
+
+if __name__ == '__main__':
+    test_cases = []
+
+    # test 0
+    test_cases.append(
+        (
+            [
+                (0, 0),
+                (1, 1),
+                (1, 2)
+            ],
+            2
+        )
+    )
+
+    # test 1
+    test_cases.append(
+        (
+            [],
+            0
+        )
+    )
+
+    # test 2
+    test_cases.append(
+        (
+            [
+                (0, 0),
+                (0, 0),
+                (0, 0),
+                (0, 0),
+                (0, 0)
+            ],
+            0
+        )
+    )
+
+    # test 3
+    test_cases.append(
+        (
+            [
+                (0, 0),
+                (10, -15), # delta: (10, 15) => 15
+                (2, 3), # delta: (8, 18) => 18
+                (10, -15), # delta: (8, 18) => 18
+                (5, 6) # delta: (5, 21) => 21
+            ],
+            15 + 18 + 18 + 21
+        )
+    )
+
+    all_passed = True
+    for idx, (test_input, expected_result) in enumerate(test_cases):
+        try:
+            result = get_min_steps_to_point(test_input)
+        except Exception:
+            print('test #{} raised an exception!'.format(idx))
+            all_passed = False
+        else:
+            if result != expected_result:
+                print('test #{} failed!'.format(idx))
+                all_passed = False
+
+    if all_passed:
+        print('All tests passed!')
