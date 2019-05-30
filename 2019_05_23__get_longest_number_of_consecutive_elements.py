f85d3372e4ef6ce1edb6a3e555288a82609a829a
lschanne <lukeschanne@gmail.com>
2019-05-30 12:05:55 -0400

    Add problem for May 23, 2019

--- 2019_05_23__get_longest_number_of_consecutive_elements.py
+++ 2019_05_23__get_longest_number_of_consecutive_elements.py
@@ -9,3 +9,29 @@ sequence is [1, 2, 3, 4]. Return its length: 4.
 
 Your algorithm should run in O(n) complexity.
 '''
+
+def get_longest_number_of_consecutive_elements(integers):
+    '''
+    # the easy, obvious, not O(n) approach:
+    max_count = 0
+    count = 0
+    prev = None
+    for x in sorted(integers):
+        if (prev is not None) and (x == prev + 1):
+            count += 1
+        else:
+            count = 1
+        if count > max_count:
+            max_count = count
+        prev = x
+    return max_count
+    '''
+    # but how the friggin' frackin' way can we do this in O(n)?
+
+if __name__ == '__main__':
+    import sys
+    integers = [int(x) for x in sys.argv[1:]]
+    print('inputs: {}'.format(integers))
+    print('output: {}'.format(
+        get_longest_number_of_consecutive_elements(integers)
+    ))
