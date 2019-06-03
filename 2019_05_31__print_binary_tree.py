c7dc54ffe30f9a5c8e941bcb231be3902f903236
lschanne <lukeschanne@gmail.com>
2019-06-03 08:51:52 -0400

    Solve the problem for May 31, 2019

--- 2019_05_31__print_binary_tree.py
+++ 2019_05_31__print_binary_tree.py
@@ -9,3 +9,32 @@ print "1, 2, 3, 4, 5":
    / \
   4   5
 '''
+
+class BinaryTree:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        string = str(self.value)
+        if self.left:
+            string += ', {}'.format(self.left)
+        if self.right:
+            string += ', {}'.format(self.right)
+        return string
+
+if __name__ == '__main__':
+    print(
+        str(
+            BinaryTree(
+                1,
+                BinaryTree(2),
+                BinaryTree(
+                    3,
+                    BinaryTree(4),
+                    BinaryTree(5),
+                )
+            )
+        )
+    )
