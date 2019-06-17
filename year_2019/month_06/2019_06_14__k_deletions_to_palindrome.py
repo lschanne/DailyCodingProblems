868c7ddbbba21b3d5e766fdc9c1d79a6878cbae1
lschanne <lukeschanne@gmail.com>
2019-06-17 10:11:32 -0400

    Solve the problem for June 14, 2019

--- year_2019/month_06/2019_06_14__k_deletions_to_palindrome.py
+++ year_2019/month_06/2019_06_14__k_deletions_to_palindrome.py
@@ -7,3 +7,29 @@ palindrome.
 For example, given 'waterrfetawx' and a k of 2, you could delete f and x to
 get 'waterretaw'.
 '''
+
+def k_deletions_to_palindrome(string, k=0):
+    def _recurse(string, k):
+        if string == string[::-1]:
+            return True
+
+        if k <= 0:
+            return False
+
+        for idx in range(len(string)):
+            if _recurse(string[:idx] + string[idx + 1:], k - 1):
+                return True
+
+        return False
+
+    return _recurse(string, k)
+
+if __name__ == '__main__':
+    import sys
+    string, k = sys.argv[1:3]
+    k = int(k)
+    print('string: {}'.format(string))
+    print('k: {}'.format(k))
+    print('can be made a palindrome: {}'.format(
+        k_deletions_to_palindrome(string, k))
+    )
