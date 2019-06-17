ce73242db738645d032b77c2a6cb31271a7b7211
lschanne <lukeschanne@gmail.com>
2019-06-17 10:57:44 -0400

    Solve the problem for June 7, 2019

--- year_2019/month_06/2019_06_07__reverse_string_by_delimiters.py
+++ year_2019/month_06/2019_06_07__reverse_string_by_delimiters.py
@@ -11,4 +11,28 @@ Follow-up: Does your solution work for the following cases:
     "hello//world:here"
 '''
 
+import re
+
+REGEX = re.compile(r'\W')
+
 # the name is a little awkward, but I feel like this task is hard to summarize
+# in a short name
+# Also, the "Follow-up" section isn't very clearly defined. What is the
+# expected result? Should "hello//world:here" treat "//" as a single
+# delimiter or should it be split into two "/" delimiters? Right now I'm doing
+# the latter. I feel like that makes more sense (maybe)
+def reverse_string_by_delimiters(string):
+    reversed_words = list(reversed(re.split(REGEX, string)))
+    delimiters = re.findall(REGEX, string)
+    result = reversed_words[0]
+    for word, delimiter in zip(reversed_words[1:], delimiters):
+        result = delimiter.join((result, word))
+    return result
+
+if __name__ == '__main__':
+    import sys
+    print('-----------------------')
+    for string in sys.argv[1:]:
+        print('string: "{}"'.format(string))
+        print('result: "{}"'.format(reverse_string_by_delimiters(string)))
+        print('-----------------------')
