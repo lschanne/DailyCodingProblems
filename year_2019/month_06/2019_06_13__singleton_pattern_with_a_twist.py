f0693d59aed841f02c4fe7d2e5753cb97e2e60b2
lschanne <lukeschanne@gmail.com>
2019-06-17 09:56:13 -0400

    Add problems for June 13-16, 2019

---
+++ year_2019/month_06/2019_06_13__singleton_pattern_with_a_twist.py
@@ -0,0 +1,10 @@
+'''
+June 13, 2019
+
+Implement the singleton pattern with a twist. First, instead of storing one
+instance, store two instances. And in every even call of getInstance(),
+return the first instance and in every odd call of getInstance(),
+return the second instance.
+'''
+
+# Right, like I just know what the singleton pattern is. Thanks, guy.
---
+++ year_2019/month_06/2019_06_14__k_deletions_to_palindrome.py
@@ -0,0 +1,9 @@
+'''
+June 14, 2019
+
+Given a string which we can delete at most k, return whether you can make a
+palindrome.
+
+For example, given 'waterrfetawx' and a k of 2, you could delete f and x to
+get 'waterretaw'.
+'''
