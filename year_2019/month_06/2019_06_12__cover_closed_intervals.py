06fc43b55cb5f94fc29a44c8f4c01c0262b53bf3
lschanne <lukeschanne@gmail.com>
2019-06-17 13:37:26 -0400

    Solve the problem for June 12, 2019

--- year_2019/month_06/2019_06_12__cover_closed_intervals.py
+++ year_2019/month_06/2019_06_12__cover_closed_intervals.py
@@ -7,3 +7,64 @@ all the intervals. If there are multiple smallest sets, return any of them.
 For example, given the intervals [0, 3], [2, 6], [3, 4], [6, 9], one set of
 numbers that covers all these intervals is {3, 6}.
 '''
+
+def cover_closed_intervals(intervals):
+    if not intervals:
+        return (None, None)
+
+    largest_start = None
+    smallest_end = None
+    for start, end in intervals:
+        if largest_start is None or start > largest_start:
+            largest_start = start
+        if smallest_end is None or end < smallest_end:
+            smallest_end = end
+
+    return (min((largest_start, smallest_end)), largest_start)
+
+if __name__ == '__main__':
+    test_cases = []
+
+    intervals = ()
+    result = (None, None)
+    test_cases.append([intervals, result])
+
+    intervals = (
+        [0, 3],
+    )
+    result = (0, 0)
+    test_cases.append([intervals, result])
+
+    intervals = (
+        [0, 3],
+        [2, 6],
+    )
+    result = (2, 2)
+    test_cases.append([intervals, result])
+
+    intervals = (
+        [0, 3],
+        [2, 6],
+        [3, 4],
+    )
+    result = (3, 3)
+    test_cases.append([intervals, result])
+
+    intervals = (
+        [0, 3],
+        [2, 6],
+        [3, 4],
+        [6, 9],
+    )
+    result = (3, 6)
+    test_cases.append([intervals, result])
+
+    for intervals, expected_result in test_cases:
+        result = cover_closed_intervals(intervals)
+        if result != expected_result:
+            raise ValueError(
+                'intervals: {}\nexpected_result: {}\nresult: {}'.format(
+                    intervals, expected_result, result
+                )
+            )
+    print('all tests passed')
